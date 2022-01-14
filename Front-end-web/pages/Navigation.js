a0b81d8bbddab4c75d7750ad17aeed05c2df1beb
Théo Sabri <60616854+theosab2@users.noreply.github.com>
2022-01-14 09:20:19 +0100

    Composant de navigation

---
+++ Front-end-web/pages/Navigation.js
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import style from '../styles/Navigation.module.css'
+
+export default function Navigation(){
+    return(
+        <div className={style.navBarContent}>
+            <Link href="/home/Home">
+            <a> 
+            <Image
+                src="/../public/Image/logo-gouvernement.jpeg" // Route of the image file
+                height={120} // Desired size with correct aspect ratio
+                width={120} // Desired size with correct aspect ratio
+                alt="Logo gouvernement français"
+            />
+            </a>
+            </Link>
+            <Link href="/home/Home">
+            <a> Créer un post</a>
+            </Link>
+            <Link href="/home/Home">
+            <a> Ressources enregistrer</a>
+            </Link>
+            <Link href="/home/Home">
+            <a> Parametre</a>
+            </Link>
+            <Link href="/home/Home">
+            <a> 
+            <Image
+                src="/../public/Image/logo-gouvernement.jpeg" // Route of the image file
+                height={70} // Desired size with correct aspect ratio
+                width={70} // Desired size with correct aspect ratio
+                alt="Image profil"
+            />
+            </a>
+            </Link>
+        </div>
+    )
+}
--- Front-end-web/pages/index.js
+++ Front-end-web/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Children } from 'react/cjs/react.production.min'
 import styles from '../styles/Home.module.css'
-
+import Navigation from './Navigation'
 export default function Home() {
   return (
-    <div>
- 
-    </div>
+    <Navigation>
+      {Children}
+    </Navigation>
   )
 }
