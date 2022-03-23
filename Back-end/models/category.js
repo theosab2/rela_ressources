b6675b666fde8ab82c66f053f563ad45dfb640dd
William-Langlois <langlois.william.pro@gmail.com>
2022-03-23 11:50:30 +0100

    reorganisation code routes + default isActive = true (category)

--- Back-end/models/category.js
+++ Back-end/models/category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
   categoryName: { type: String, required: true, unique: true },
-  categoryIsActive: { type: Boolean, required: true},
+  categoryIsActive: { type: Boolean, required: true, default:true},
 });
 
 module.exports = mongoose.model('Category', categorySchema);
