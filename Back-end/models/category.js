83e55706ec80a2860650dcd0ba8e5840f27cfb8d
William-Langlois <langlois.william.pro@gmail.com>
2022-03-31 19:32:21 +0200

    Suppression model category

--- Back-end/models/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = mongoose.Schema({
-  categoryName: { type: String, required: true, unique: true },
-  categoryIsActive: { type: Boolean, required: true, default:true},
-});
-
-module.exports = mongoose.model('Category', categorySchema);
