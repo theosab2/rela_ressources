fc6aa056d4dca337544b5e98e95fa39046ca5408
Théo Sabri <theo27400@live.fr>
2022-05-01 13:25:50 +0200

    Amélioration de l'espace de navigation

--- Front-end-web/pages/Navigation.js
+++ Front-end-web/pages/Navigation.js
@@ -34,13 +34,35 @@ export default function Navigation(image) {
   if (userCookie == false) {
     return (
       <>
-        <div className={style.header}>
-        <img src="/Image/burger-menu.png" className={style.burger_menu}/>
-        <p className={style.headerTitle}>Ressource Relationnelle</p>
+        <div className={style.navHeader}>
+          <img src="/Image/burger-menu.png" className={style.burger_menu}/>
+          <p className={style.headerTitle}>Ressource Relationnelle</p>
+          <img src="/Image/connexion.png" className={style.icon_connexion}/>
         </div>
-        <div className={style.sideBar}>
-          <div className={style.sidebarcontent}>
-
+        <div className={style.navBody}>
+          <div className={style.sideBar}>
+            <div className={style.sidebarContent}>
+              <div className={style.sidebarTitle}>Profil</div>
+              <div className={style.sidebarChoice}>
+                <div>Connexion</div>
+                <div>Inscription</div>
+              </div>
+            </div>
+          </div>
+          <div className={style.recentContent}>
+            <div className={style.recentTitle}>Récent</div>
+            <div className={style.recentChoice}>
+                <img 
+                src="/Image/connexion.png"
+                alt="User picture"
+                className={style.recentUserPicture}
+                />
+                <div className={style.recentInfo}>
+                  <div className={style.recentUserName}>JeanMichel62</div>
+                  <div className={style.recentArticleTitle}>La terre est ovoidale</div>
+              </div>
+            </div>
+          
           </div>
         </div>
       </>
