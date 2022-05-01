8cbbee308ff5c96dd5a61235173558b99b1340c5
William-Langlois <langlois.william.pro@gmail.com>
2022-05-01 14:11:30 +0200

    remove role model

--- Back-end/models/role.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = mongoose.Schema({
-  roleName: { type: String, required: true, unique:true},
-  type: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Role', roleSchema);
