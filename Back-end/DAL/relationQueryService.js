e5ed15abe6005a285c3ef6d951b4f8d8315a0fc3
William-Langlois <langlois.william.pro@gmail.com>
2022-05-09 15:06:34 +0200

    API-relation-v1

--- Back-end/DAL/relationQueryService.js
+++ Back-end/DAL/relationQueryService.js
@@ -8,32 +8,32 @@ const _typeValidationService = require("../BLL/global/typeValidationService");
 
 
 //Import du modèle relatif
-const mMessage = require('../models/message');
+const mRelation = require('../models/relation');
 
-const queryServiceLogPrefix = "    (message)      D.A.L ";
+const queryServiceLogPrefix = "    (relation)      D.A.L ";
 
 module.exports.getSchemaPath = async () => {
     console.log(queryServiceLogPrefix,"[getSchemaPath] ()");
     console.log(queryServiceLogPrefix,"[getSchemaPath] (return) schemaPath");
-    return Object.entries(mMessage.schema.paths);
+    return Object.entries(mRelation.schema.paths);
 };
 
 module.exports.getAll = async () => {
     console.log(queryServiceLogPrefix,"[getAll] ()");
 
     try {
-        var data = await mMessage.find();
+        var data = await mRelation.find();
         if(data.length > 0){
             console.log(queryServiceLogPrefix,"[getAll] (return) ",data.length," element",data.length > 1 ?'s':'');
             return {
-                messages:data,
+                relations:data,
                 count:data.length
             };
 
         }else{
-            console.log(queryServiceLogPrefix,"[getAll] (return) 0 element : no message found");
+            console.log(queryServiceLogPrefix,"[getAll] (return) 0 element : no relation found");
             return {
-                message:"aucun message enregistré en base de données"
+                message:"aucune relation enregistrée en base de données"
             };
 
         }       
@@ -61,13 +61,13 @@ module.exports.query = async (query = {}) => {
         parsedQuery = await _queryParserService.parseObjectQuery(query);
         
         try {
-            var data = await mMessage.find().where(query);
+            var data = await mRelation.find().where(query);
             var result = 
             data ? {
-                messages:data,
+                relations:data,
                 count:data.length
             } : {
-                message:"aucun message trouvé en base de données correspondant à la query"
+                message:"aucune relation trouvée en base de données correspondant à la query"
             };
             console.log(queryServiceLogPrefix,"[query] (return) : ",data.length," element",data.length > 1 ?'s':'');
             return result 
@@ -80,11 +80,11 @@ module.exports.query = async (query = {}) => {
     }
 };
 
-module.exports.saveOne = async (newMessageModel) => {
+module.exports.saveOne = async (newRelationModel) => {
     try //Création du modèle à partir des données du body de la requête
     {
         console.log(queryServiceLogPrefix,"[create] (info) creating message object before database insertion");
-        var newMessage = new mMessage({ ...newMessageModel });
+        var newRelation = new mRelation({ ...newRelationModel });
     }
     catch (exception) //ECHEC Création du modèle à partir des données du body de la requête
     {   
@@ -92,10 +92,26 @@ module.exports.saveOne = async (newMessageModel) => {
         return ({
             status:"EXCEPTION",
             statusCode:500,
-            message: "Une erreur est survenue durant la création du modèle pour le nouvel article : \'"+newMessageModel.name+"\'",
-            messageInfoReceipted:messageObject,
+            message: "Une erreur est survenue durant la création du modèle pour le nouvel article : \'"+newRelationModel.name+"\'",
+            relationInfoReceipted:newRelationModel,
             exception:exception
         })
     }
-    newMessage.save()
+
+    try //Création du modèle à partir des données du body de la requête
+    {
+        console.log(queryServiceLogPrefix,"[create] (info) saving created relation object in database");
+        newRelation.save()
+    }
+    catch (exception) //ECHEC Création du modèle à partir des données du body de la requête
+    {   
+        console.log(queryServiceLogPrefix,"[create] (error) exception occur during relation object save in database : ",exception);
+        return ({
+            status:"EXCEPTION",
+            statusCode:500,
+            message: "Une erreur est survenue durant la création du modèle pour le nouvel article : \'"+newRelationModel.name+"\'",
+            relationInfoReceipted:newRelationModel,
+            exception:exception
+        })
+    }
 }
