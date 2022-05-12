eb4138a7085f775bde49e8aa179cda8695963298
Samson <51507673+MrSamafu@users.noreply.github.com>
2022-05-12 20:52:58 +0200

    createPost front

--- Front-end-mobile/App.js
+++ Front-end-mobile/App.js
@@ -13,195 +13,15 @@ import Home from './screens/HomeStack/home';
 import Login from './screens/AuthStack/login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import CreateAccount from './screens/AuthStack/createAccount';
 import Account from './screens/AuthStack/account';
-import Header from './components/header';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-elements';
 import CreatePost from './screens/AddStack/createPost';
 import PostSaved from './screens/SavedStack/postSaved';
-import { Button } from 'react-native-elements/dist/buttons/Button';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import Post from './screens/HomeStack/post';
 import IdSetting from './screens/AuthStack/idSetting';
 import PwdSetting from './screens/AuthStack/pwdSetting';
 import CoorSetting from './screens/AuthStack/coorSetting';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-const StackAccount = ({ navigation, route }) => {
-  const [userId, setUserId] = useState(null);
-
-
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const storedId = await AsyncStorage.getItem('@userId');
-        setUserId(storedId != null ? storedId : null)
-      } catch (e) {
-      }
-    }
-    getData();
-  }, [])
-
-  if (userId != null) {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: true
-        }}>
-        <Stack.Screen name='account'
-          component={Account}
-          options={{
-            headerLeft: props => (<Icon name='menu'
-              type='ionicon'
-              color='#869ECE'
-              size={40}
-              onPress={() => navigation.openDrawer()} />),
-            title: 'Profile'
-          }}
-        />
-      </Stack.Navigator>
-    )
-  }
-  else {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false
-        }}>
-        <Stack.Screen name='Login'
-          component={Login}
-        />
-        <Stack.Screen name='createAccount'
-          component={CreateAccount}
-        />
-      </Stack.Navigator>
-    )
-
-  }
-}
-const TabNavigation = ({ navigation, route }) => {
-  const [connected, setConnect] = useState(false);
-  React.useEffect(() => {
-    if (route.params?.post) {
-      setConnect(true);
-    }
-  }, [route.params?.post]);
-
-  const connexion = () => {
-    console.log(connected)
-  }
-  return (
-    <Tab.Navigator
-      initialRouteName='home'
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color }) => {
-          let iconName;
-          if (route.name === 'home') {
-            iconName = focused
-              ? 'newspaper'
-              : 'newspaper-outline';
-          }
-          if (route.name === 'createPost') {
-            iconName = focused
-              ? 'add-circle'
-              : 'add-circle-outline';
-          }
-          if (route.name === 'postSaved') {
-            iconName = focused
-              ? 'folder-open'
-              : 'folder-outline';
-          }
-
-          // You can return any component that you like here!
-          return <Icon name={iconName}
-            type='ionicon'
-            color={color} />;
-        },
-        tabBarActiveTintColor: '#2F4077',
-        tabBarInactiveTintColor: '#869ECE',
-      })}
-    >
-      <Tab.Screen name='home'
-        component={Home}
-        options={{
-          headerLeft: props => (<Icon name='menu'
-            type='ionicon'
-            color='#869ECE'
-            size={40}
-            onPress={() => navigation.openDrawer()} />),
-          headerRight: () => (<Icon name='person-circle-outline'
-            type='ionicon'
-            color='#869ECE'
-            size={40}
-            onPress={() => navigation.navigate('Login')} />),
-          title: 'Fil d\'actus'
-        }}
-      />
-      <Tab.Screen name='createPost'
-        component={CreatePost}
-        options={{
-          headerLeft: props => (<Icon name='menu'
-            type='ionicon'
-            color='#869ECE'
-            size={40}
-            onPress={() => navigation.openDrawer()} />),
-          headerRight: () => (<Icon name='person-circle-outline'
-            type='ionicon'
-            color='#869ECE'
-            size={40}
-            onPress={() => navigation.navigate('Login')} />),
-          title: 'Créer une ressource'
-        }}
-      />
-      <Tab.Screen name='postSaved'
-        component={PostSaved}
-        options={{
-          headerLeft: () => (<Icon name='menu'
-            type='ionicon'
-            color='#869ECE'
-            size={40}
-            onPress={() => navigation.openDrawer()} />),
-          headerRight: () => (<Icon name='person-circle-outline'
-            type='ionicon'
-            color='#869ECE'
-            size={40}
-            onPress={() => navigation.navigate('Login')} />),
-          title: 'Vos ressources'
-        }}
-
-      />
-    </Tab.Navigator>
-  )
-}
-const DrawerNavigation = ({ navigation, route }) => {
-  return (
-    <Drawer.Navigator initialRouteName='Home'>
-      <Drawer.Screen
-        name='Home'
-        component={TabNavigation}
-        options={{
-          title: 'Accueil',
-          headerShown: false,
-        }}
-
-      />
-      <Drawer.Screen
-        name='Login'
-        component={StackAccount}
-        options={{
-          title: "Compte",
-          headerShown: false
-        }}
-      />
-    </Drawer.Navigator>
-  )
-}
 const HomeStack = ({ navigation, route }) => {
   return (
     <Stack.Navigator
@@ -236,7 +56,7 @@ const AddStack = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: true
+        headerShown: false
       }}>
       <Stack.Screen
         name='CreatePost'
--- Front-end-mobile/screens/AddStack/createPost.js
+++ Front-end-mobile/screens/AddStack/createPost.js
@@ -1,19 +1,25 @@
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import React, { useState } from 'react';
-import { Button, Input, Image } from 'react-native-elements';
-import LinearGradient from 'react-native-linear-gradient';
 import DocumentPicker from 'react-native-document-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {API_URL} from "@env"
+import { API_URL } from "@env"
+import Header from '../../components/header'
+import { Divider } from 'react-native-elements';
+import HomemadeNavBar from '../../components/homemadeNavBar';
+import { Picker } from '@react-native-picker/picker';
+import { Icon, Button, Image } from 'react-native-elements';
 
-const CreatePost = () => {
-  const [articleTitle, setArticleTitle] = useState('');
-  const [articleDescritpion, setArticleDescription] = useState('');
-  const [articleTTags, setArticleTTags] = useState([]);
-  const [articleCategory, setArticleCategory] = useState(undefined);
-  const [articleContentIds, setArticleContentIds] = useState([]);
-  const [articleContents, setArticleContents] = useState([])
-  const [articleCreator, setArticleCreator] = useState('');
+
+
+const CreatePost = ({ navigation }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [namePicture, setNamePicture] = useState('');
+  const [tTags, setTTags] = useState([]);
+  const [category, setCategory] = useState(undefined);
+  const [contentIds, setContentIds] = useState([]);
+  const [contents, setContents] = useState([])
+  const [creator, setCreator] = useState('');
   const [singleFile, setSingleFile] = useState(null);
   const [imageUri, setImageUri] = useState(null);
 
@@ -61,7 +67,7 @@ const CreatePost = () => {
       articleCreator: user,
     })
     formdata.append("article", jsonData);
-    try{
+    try {
       const api = await fetch(API_URL + '/article/create', {
         method: 'POST',
         headers: {
@@ -72,97 +78,211 @@ const CreatePost = () => {
       });
       const res = await api.json();
       console.log(res);
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
   }
   return (
-    <LinearGradient
-      colors={['#869ece', '#ffffff']}
-      style={styles.linearGradient}
-      locations={[0, 1]}
-    >
-      <Input
-        placeholder='Titre'
-        onChangeText={articleTitre => setArticleTitle(articleTitre)}
-        defaultValue={articleTitle}
-      />
-      <Input
-        placeholder='Description'
-        onChangeText={articleDescription => setArticleDescription(articleDescription)}
-        defaultValue={articleDescritpion}
-      />
-      <Input
-        placeholder='Tags'
-        onChangeText={articleTTags => setArticleTTags(...articleTTags)}
-        defaultValue={articleTTags.join(' ')}
-      />
-      {singleFile != null ? (
-        <View>
-          <Image
-            style={styles.uploadImg}
-            source={imageUri != null ? {uri: imageUri} : require('../../test_content/waiting.jpg')}
-          />
-          <Text >
-          File Name: {singleFile.name ? singleFile.name : ''}
-          {'\n'}
-          Type: {singleFile.type ? singleFile.type : ''}
-          {'\n'}
-          File Size: {singleFile.size ? singleFile.size : ''}
-          {'\n'}
-          URI: {singleFile.uri ? singleFile.uri : ''}
-          {'\n'}
-          </Text>
+    <View style={styles.container}>
+      <Header navigation={navigation} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Créer votre ressource</Text>
+        <Divider
+          orientation='horizontal'
+          color='#CE8686'
+          style={styles.divider}
+        />
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={"Coucou"}
+            style={styles.picker}
+            placeholder="Catégorie"
+          >
+            <Picker.Item label="Catégorie" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
         </View>
-      ) : null}
-      <TouchableOpacity
-        style={styles.buttonStyle}
-        activeOpacity={0.5}
-        onPress={selectFile}>
-        <Text style={styles.buttonTextStyle}>Select File</Text>
-      </TouchableOpacity>
-      <Button
-        title='Valider'
-        onPress={sendFile}
-      />
-    </LinearGradient>
+
+        <View style={styles.input}>
+          <Icon name="text-outline" type="ionicon" color="#FFFFFF" style={styles.inputIcon} />
+          <TextInput
+            textContentType='title'
+            placeholder="Titre"
+            onChangeText={title => setTitle(title)}
+            defaultValue={title}
+
+          />
+        </View>
+        <View style={[styles.input, styles.inputDescription]}>
+          <Icon name="receipt-outline" type="ionicon" color="#FFFFFF" style={styles.inputIcon} />
+          <TextInput
+            textContentType='description'
+            placeholder="Courte description décrivant votre ressource"
+            onChangeText={description => setDescription(description)}
+            defaultValue={description}
+            multiline={true}
+            style={styles.textArea}
+          />
+        </View>
+        <View style={styles.imagePickerContainer}>
+          <View style={styles.imagePickerLeft}>
+            <View style={[styles.input, styles.inputPictureTitle]}>
+              <Icon name="image-outline" type="ionicon" color="#FFFFFF" style={styles.inputIcon} />
+              <Text>{namePicture == '' ? "Sélectionner une image de couverture" : namePicture}</Text>
+            </View>
+            <View style={styles.buttonPictureContainer}>
+              <Button
+                title="Supprimer"
+                buttonStyle={[styles.buttonStyle, styles.buttonDeletePicture]}
+                containerStyle={styles.buttonContainerStyle}
+                activeOpacity={0.5}
+                onPress={selectFile}
+              />
+              <Button
+                title="Ajouter une image"
+                buttonStyle={styles.buttonStyle}
+                containerStyle={styles.buttonContainerStyle}
+                activeOpacity={0.5}
+                onPress={selectFile}
+              />
+            </View>
+          </View>
+          <View style={styles.imagePicker}>
+            <Image
+              style={styles.uploadImg}
+              source={imageUri == null ? require('../../test_content/waiting.jpg') : require('../../test_content/waiting.jpg')}
+            />
+          </View>
+        </View>
+      </View>
+      <View style={styles.buttons}>
+        <Button
+          title="Continuer"
+          loading={false}
+          loadingProps={{ size: 'small', color: 'white' }}
+          buttonStyle={styles.buttonStyle}
+          titleStyle={{ fontWeight: 'bold', fontSize: 12 }}
+          containerStyle={[styles.buttonContainerStyle, styles.buttonContinue]}
+          onPress={() => {
+            validation();
+          }}
+        />
+      </View>
+      <View style={styles.footer}>
+      </View>
+      <HomemadeNavBar navigation={navigation} />
+    </View>
   );
 };
 
-export default CreatePost;
-
 const styles = StyleSheet.create({
-  linearGradient: {
-    height: '100%',
+  container: {
+    flex: 1,
+    backgroundColor: '#2F4077'
+  },
+  scrollViewContainer: {
+    flex: 12,
+  },
+  scrollView: {
+    flex: 8,
+  },
+  postPosition: {
+    alignItems: 'center'
+  },
+  titleContainer: {
     width: '100%',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  divider: {
+    width: '33%',
+    margin: 10
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 10
+  },
+  pickerContainer: {
+    width: "95%",
+    height: 60,
+    backgroundColor: "#869ECE",
+    margin: 5,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  picker: {
+    width: "100%",
+    height: 60,
+  },
+  input: {
+    backgroundColor: "#869ECE",
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 5,
+    width: "95%",
+  },
+  inputDescription: {
+    height: 150,
+    alignItems: 'flex-start',
+
+  },
+  inputIcon: {
+    margin: 5,
+  },
+  textArea: {
+    flex: 1,
+    flexShrink: 1,
+    textAlignVertical: 'top',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   buttonStyle: {
-    backgroundColor: '#307ecc',
-    borderWidth: 0,
-    color: '#FFFFFF',
-    borderColor: '#307ecc',
-    height: 40,
+    backgroundColor: '#CE8686',
+    borderRadius: 10,
+    height: 50,
+  },
+  buttonContainerStyle: {
+    margin: 2,
+    width: '90%',
+  },
+  buttonDeletePicture: {
+    backgroundColor: '#BEC4D3'
+  },
+  imagePickerContainer: {
+    flexDirection: 'row',
+    width: "95%",
+    height: 170,
+    backgroundColor: '#869ECE',
+    borderRadius: 10,
+    margin: 5,
+  },
+  imagePickerLeft: {
+    flexDirection: 'column',
+    width: "66%"
+  },
+  imagePicker: {
+    width: "33%",
+    height: "100%",
+    justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 30,
-    marginLeft: 35,
-    marginRight: 35,
-    marginTop: 15,
   },
-  buttonTextStyle: {
-    color: '#FFFFFF',
-    paddingVertical: 10,
-    fontSize: 16,
-  },
-  textStyle: {
-    backgroundColor: '#fff',
-    fontSize: 15,
-    marginTop: 16,
-    marginLeft: 35,
-    marginRight: 35,
-    textAlign: 'center',
+  buttonPictureContainer: {
+    alignItems: 'center',
   },
   uploadImg: {
-    width: 100,
-    height: 100,
+    width: 125,
+    height: 125,
+    borderRadius: 10,
+  },
+  buttonContinue: {
+    width: "95%",
+    margin: 5
   }
+
 });
+export default CreatePost;
