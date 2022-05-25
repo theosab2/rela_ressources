fc24c9032c488d65ce5e139e3d008c64359f4202
Samson <51507673+MrSamafu@users.noreply.github.com>
2022-05-25 20:33:12 +0200

    Front account

--- Front-end-mobile/screens/AuthStack/account.js
+++ Front-end-mobile/screens/AuthStack/account.js
@@ -1,22 +1,29 @@
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions,ScrollView, SafeAreaView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Icon} from 'react-native-elements';
+import { Divider, Icon } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import {API_URL} from "@env"
+import { API_URL } from "@env"
 import HomemadeNavBar from '../../components/homemadeNavBar';
 import Header from '../../components/header';
+import Card from '../../components/card';
 
-const Account = ({navigation}) => {
+
+const Account = ({ navigation }) => {
   const [userData, setUserData] = useState({});
-  
+  const[selectedTab,setSelectedTab] = useState('top');
+  /**
+   * A modifier => Créer deux state pour new et top ressource
+   */
+  const [display, setDisplay] = useState(<Text>Loading</Text>);
+
   useEffect(() => {
     const getLocalData = async () => {
       try {
         const storedId = await AsyncStorage.getItem('@userId');
-        if(storedId != null){
+        if (storedId != null) {
           getUserData(storedId)
-        }else{
+        } else {
           //navigation.navigate('Auth', {screen: 'Login'})
         }
       } catch (e) {
@@ -33,24 +40,75 @@ const Account = ({navigation}) => {
           },
         });
         const res = await api.json();
-        console.log(res);
         setUserData(res);
       } catch (e) {
         console.log(e);
       }
-    } 
+    }
+    const getPost = async () => {
+      try{
+        const api = await fetch(API_URL + '/articles/all', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+        });
+        const res = await api.json()
+        setDisplay(displayPost(res));
+      }catch(e){
+        console.log(e);
+      }
+    }
+    const displayPost = (data) => {
+      if(data.articles != undefined){
+        return data.articles.map(item => {
+          return <Card navigation={navigation} key={item._id} data={item}/>
+        })
+      }else{
+        return <View/>
+      }
+    }
+    getPost();
     getLocalData();
   }, []);
 
 
-  
+
 
   return (
     <View style={styles.container}>
-      <Header navigation={navigation}/>
+      <Header navigation={navigation} />
       <View style={styles.toolsButtons}>
+        <TouchableOpacity
+          style={styles.tool}
+        >
+          <Icon
+            name='pencil-outline'
+            type='ionicon'
+            color='#FFFFFF'
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.tool}
+        >
+          <Icon
+            name='settings-outline'
+            type='ionicon'
+            color='#FFFFFF'
+          />
+        </TouchableOpacity>
+      </View>
+      <View
+        style={styles.card}
+      >
+        <Image
+          style={styles.profilImage}
+          source={require('../../test_content/zombie.png')}
+        />
+        <View style={styles.overlayImage}>
           <TouchableOpacity
-            style={styles.tool}
+            style={styles.profilImageSetting}
           >
             <Icon
               name='pencil-outline'
@@ -58,64 +116,52 @@ const Account = ({navigation}) => {
               color='#FFFFFF'
             />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.tool}
-          >
-            <Icon
-              name='settings-outline'
-              type='ionicon'
-              color='#FFFFFF'
-            />
-          </TouchableOpacity>
-      </View>
-        <View
-          style={styles.card}
-        >
-          <View style={styles.profilCadre}>
-            <Image
-              style={styles.profilImage}
-              source={require('../../test_content/zombie.png')}
-            />
+        </View>
+        <Text style={styles.username}>{userData.username}</Text>
+        <Text style={styles.name}>{userData.name} {userData.firstname}</Text>
+        <View style={styles.containerStats}>
+          <View style={styles.containerStat}>
+            <Text style={styles.stat}>42</Text>
+            <Text style={styles.libelleStat}>Ressources</Text>
           </View>
-          <Text style={styles.username}>{userData.username}</Text>
-          <Text style={styles.name}>{userData.name} {userData.firstname}</Text>
-          <Text style={styles.text}>{userData.email}</Text>
-          <Text style={styles.text}>{userData.phone}</Text>
-          <Text style={styles.text}>{userData.role}</Text>
+          <Divider
+            orientation='vertical'
+            color='#CE8686'
+            style={styles.divider}
+          />
+          <View style={styles.containerStat}>
+            <Text style={styles.stat}>128</Text>
+            <Text style={styles.libelleStat}>Relations</Text>
+          </View>
         </View>
-        <View style={styles.buttonList}>
-          <TouchableOpacity containerStyle={styles.buttonContainerLeft} style={styles.buttonLeft}>
-            <Icon
-              name='people'
-              type='ionicon'
-              color='#bec4d3'
-            />
-            <Text style={styles.text}>Relations</Text>
-          </TouchableOpacity>
-          <TouchableOpacity containerStyle={styles.buttonContainer} style={styles.button}>
-            <Icon
-              name='pencil'
-              type='ionicon'
-              color='#bec4d3'
-            />
-            <Text style={styles.text}>Créations</Text>
-          </TouchableOpacity>
-          <TouchableOpacity containerStyle={styles.buttonContainerRight} style={styles.buttonRight}>
-            <Icon
-              name='chatbox-ellipses'
-              type='ionicon'
-              color='#bec4d3'
-            />
-            <Text style={styles.text}>Messages</Text>
-          </TouchableOpacity>
-        </View>
-        <HomemadeNavBar route='Relation' navigation={navigation}/>
+      </View>
+      <View style={styles.buttonList}>
+        <TouchableOpacity
+          containerStyle={styles.containerRessourceType}
+          style={[styles.ressourceType,selectedTab == 'top' ? styles.selectedRessource : null]}
+          onPress={() => setSelectedTab('top')}
+        >
+          <Text style={styles.ressourceTitle}>Top ressources</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          containerStyle={styles.containerRessourceType}
+          style={[styles.ressourceType,selectedTab == 'new' ? styles.selectedRessource : null]}
+          onPress={()=> setSelectedTab('new')}
+        >
+          <Text style={styles.ressourceTitle}>Les plus récentes</Text>
+        </TouchableOpacity>
+      </View>
+      <SafeAreaView style={styles.scrollViewContainer}>
+        <ScrollView style={styles.scrollView} contentContainerStyle={styles.postPosition}>
+          {display} 
+        </ScrollView>
+      </SafeAreaView>
+      <HomemadeNavBar route='Relation' navigation={navigation} />
     </View>
   );
 };
 
 export default Account;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -146,53 +192,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  buttonList:{
-    height: '20%',
+  buttonList: {
     width: '95%',
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
     backgroundColor: '#2F4077',
-    flexDirection:'row',
-  },
-  buttonContainer:{
-    flex: 1,
-    
-  },
-  buttonContainerLeft:{
-    flex: 1,
-    borderBottomLeftRadius: 10,
-  },
-  buttonContainerRight:{
-    flex: 1,
-    borderBottomRightRadius: 10,
-  },
-  button: {
-    flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#869ece',
-    borderWidth: 1,
-    borderTopWidth:0,
   },
-  buttonLeft: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#869ece',
-    borderWidth: 1,
-    borderTopWidth:0,
-    borderBottomLeftRadius: 10
-  },
-  buttonRight: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#869ece',
-    borderWidth: 1,
-    borderTopWidth:0,
-    borderBottomRightRadius: 10
-  },
-  username:{
+  username: {
     fontSize: 30,
     fontWeight: 'bold'
   },
@@ -202,30 +210,72 @@ const styles = StyleSheet.create({
   text: {
     margin: 10
   },
-  profilCadre:{
-    borderTopLeftRadius: 500,
-    borderBottomLeftRadius:500,
-    borderBottomRightRadius:500,
-    borderTopRightRadius: 500,
-    borderColor: '#869ece',
-    borderWidth:3,
-    width: 215,
-    height:215,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  profilImage:{
-    borderTopLeftRadius: 500,
-    borderBottomLeftRadius:500,
-    borderBottomRightRadius:500,
-    borderTopRightRadius: 500,
-    width: 200,
-    height:200
+  profilImage: {
+    borderRadius: 100,
+    width: 150,
+    height: 150,
+    margin: 25,
   },
   linearGradient: {
     alignItems: 'center',
     justifyContent: 'center',
     height: '100%',
     width: '100%',
-  }
+  },
+  overlayImage: {
+    flex: 1,
+    position: 'absolute',
+    top: 150,
+    left: Dimensions.get('window').width / 2 + 15,
+  },
+  profilImageSetting: {
+    width: 35,
+    height: 35,
+    backgroundColor: '#CE8686',
+    margin: 5,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 100
+  },
+  containerStats: {
+    margin: 15,
+    width: "50%",
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  containerStat: {
+    alignItems: 'center',
+    margin: 5
+  },
+  stat: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  libelleStat: {
+    fontSize: 12,
+  },
+  containerRessourceType: {
+    width: "50%",
+    alignItems: 'center',
+  },
+  ressourceType:{
+    borderBottomWidth: 1,
+    width: "100%",
+  },
+  ressourceTitle: {
+    width: "100%",
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: 5
+  },
+  selectedRessource:{
+    borderBottomWidth: 3,
+    borderBottomColor: '#CE8686'
+  },
+  postPosition: {
+    alignItems: 'center'
+  },
+
+
 });
