93384597d1150f297d52b2dbc72e5233902f778d
William-Langlois <langlois.william.pro@gmail.com>
2022-06-22 19:41:53 +0200

    fix

--- Back-end/MiddleWares/multer-configuration.js
+++ Back-end/MiddleWares/multer-configuration.js
@@ -1,4 +1,5 @@
 const _multer = require('multer');
+const __artContentsDirname = "./Assets/Uploads/Image/Article/";
 
 const MIME_TYPES = {
     'image/jpg' : 'jpg',
@@ -40,3 +41,24 @@ const userAvatarStorage = _multer.diskStorage({
     },
 });
 module.exports.avatarImage = _multer({storage: userAvatarStorage}).single('avatar-image');
+
+const contentMediaStorage = _multer.diskStorage({
+
+        destination: (req, file, callback) => {
+            callback(null, path.join(`${__dirname}/../../upload`));
+        },
+        filename: (req, file, callback) => {
+            const match = ["image/png", "image/jpeg"];
+            if (match.indexOf(file.mimetype) === -1) {
+                var message = `${file.originalname} is invalid. Only accept png/jpeg.`;
+                return callback(message, null);
+            }
+            const fileName = req.body.article.title;
+            const fileExtension = MIME_TYPES[file.mimetype];
+            const dateTimeGUID = Date.now();
+            const userGUID = request.header("user-upload-GUID");
+
+            callback(null, `${dateTimeGUID}@${userGUID}__${fileName}.${fileExtension}`);
+        },
+    });
+module.exports.contentMedias = _multer({storage: contentMediaStorage}).array('content-medias',25);
