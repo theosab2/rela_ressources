a7d54411c43b3849ad2a1a9e39cd9ea5466c5dbd
Théo Sabri <theo27400@live.fr>
2022-07-07 11:55:45 +0200

    test fix

--- Front-end-web/pages/login/ComponentConnexion.js
+++ Front-end-web/pages/login/ComponentConnexion.js
@@ -4,7 +4,7 @@ import { useState,useEffect } from "react";
 
   
 export default async function ComponentConnexion(email,password) {
-  //const [connection,setConnection] = useState(null);
+    if(email != null && password != null){
     let res = await fetch("http://"+process.env.IP+":3001/auth/login", {
       method: "POST",
       headers: {
@@ -16,17 +16,16 @@ export default async function ComponentConnexion(email,password) {
         password: password,
       }),
     });
+
     res = await res.json();
-    console.log(res.status)
     if (res.status != "SUCCESS" || res.user.isActive == false) {
-      console.log(res);
       if(res.status == null || res.status != "SUCCESS"){
         return "Mauvais identifiant ou mot de passe";
       }else if(res.user.isActive == false){
         return "Votre compte a été bloqué";
       }
     } else {
-      console.log("Connexion",res.user);
       setCookies("token", res.user, 1 * 3600);
     }
+  }
 }
