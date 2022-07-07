f7b2e9724dc8f97e72d4e7a7a35f87c3dab0011c
Théo Sabri <theo27400@live.fr>
2022-07-07 11:09:26 +0200

    fix

--- Front-end-web/pages/home/ComponentJoinGroupe.js
+++ Front-end-web/pages/home/ComponentJoinGroupe.js
@@ -21,7 +21,7 @@ export default function ComponentJoinGroupe(props) {
           },
           body: JSON.stringify({
             relation: {
-              user_ids: array
+              "user_ids": array
             },
           }),
         });
@@ -48,7 +48,9 @@ export default function ComponentJoinGroupe(props) {
     }
 
     useEffect(function showPost(){
+      if(props.group != null){
       setTxtAbo(props.group.user_ids.includes(userCookie._id));
+      }
       if(id != null){
         if (window) { 
           window.sessionStorage.setItem("Page", "EnterGroup" );
@@ -58,7 +60,7 @@ export default function ComponentJoinGroupe(props) {
       }
   },[id]);
     
-    if(props.group.user_ids != null){
+    if(props.group != null ){
     return (
         <div className={style.groupeContainer} >
             <div className={style.groupContainerImage}>
