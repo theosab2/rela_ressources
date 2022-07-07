3e93a366e1e52f4b21588bc2e99a5d002b56fd0c
Théo Sabri <theo27400@live.fr>
2022-07-07 13:16:20 +0200

    fix

--- Front-end-web/test/Connexion.test.js
+++
@@ -1,5 +0,0 @@
-const allArticle = require('../pages/login/ComponentConnexion');
-
-test('allArticle get all article', () => {
-  expect(() => ComponentConnexion("Mauvais mail","Mauvais mdp")).toEqual("Mauvais identifiant ou mot de passe");
-});
