54fde9c02cfa8ba63b07ef94c266f7c5e2afa2dd
William-Langlois <langlois.william.pro@gmail.com>
2022-07-08 11:51:46 +0200

    try-fix-cors

--- Back-end/app.js
+++ Back-end/app.js
@@ -47,9 +47,6 @@ module.exports = function (app, server) {
         });
     }, 100); //Timeout to log DB connection status after listened port's log
   }
-  const cors = require("cors");
-  const corsOptions = { origin: ["*"], credentials: true }
-  app.use(cors(corsOptions));
 
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
